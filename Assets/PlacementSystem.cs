f0208b78e66fc4c480aaf9ea2681d6698947cc68
gitsleuth <154627596+gitsleuth@users.noreply.github.com>
2024-07-06 14:56:36 +0100

    Create Player Health Controller

--- Assets/PlacementSystem.cs
+++ Assets/PlacementSystem.cs
@@ -10,7 +10,7 @@ public class PlacementSystem : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
---
+++ Assets/PlayerHealthController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    private float playerHealth = 100;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+
+        if (playerHealth <= 0)
+        {
+            OnPlayerDead();
+        }
+    }
+
+    // We'll use this later
+    public void OnPlayerDead()
+    {
+
+    }
+}

c102bfea4a7e741dfaf77702812fdbbb8b79eb3e
gitsleuth <154627596+gitsleuth@users.noreply.github.com>
2024-07-08 20:11:46 +0100

    Make it so the player can't exit the screen

--- Assets/PlayerCharacterController.cs
+++ Assets/PlayerCharacterController.cs
@@ -16,12 +16,37 @@ public class PlayerCharacterController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
+        float characterLeft = (playerCharacterTrans.position + Vector3.left * playerCharacterTrans.localScale.x / 2).x;
+        float screenLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 10)).x;
+
+        float characterRight = (playerCharacterTrans.position + Vector3.right * playerCharacterTrans.localScale.x / 2).x;
+        float screenRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 10)).x;
+
+        float characterTop = (playerCharacterTrans.position + Vector3.up * playerCharacterTrans.localScale.y / 2).y;
+        float screenTop = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 10)).y;
+
+        float characterBottom = (playerCharacterTrans.position + Vector3.down * playerCharacterTrans.localScale.x / 2).y;
+        float screenBottom = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 10)).y;
+
+        if (characterLeft <= screenLeft)
+        {
+            playerCharacterTrans.position += Vector3.right * Mathf.Abs(characterLeft - screenLeft);
+        } else if (characterRight >= screenRight)
+        {
+            playerCharacterTrans.position += Vector3.left * Mathf.Abs(characterRight - screenRight);
+        }
+
+        if (characterTop >= screenTop)
+        {
+            playerCharacterTrans.position += Vector3.down * Mathf.Abs(characterTop - screenTop);
+        } else if (characterBottom <= screenBottom)
+        {
+            playerCharacterTrans.position += Vector3.up * Mathf.Abs(characterBottom - screenBottom);
+        }
     }
 
     public void MovePlayer(Vector3 direction, float dt, float speed)
     {
-        print(speed);
         playerCharacterTrans.position += direction * dt * speed;
     }
 }
